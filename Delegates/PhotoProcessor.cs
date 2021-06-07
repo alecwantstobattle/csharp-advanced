10d9fa5007b9ca029dc6c105e1f7c6af6a78acc9
Alec Tecson <tecsonalr@gmail.com>
2021-06-07 19:43:59 +0800

    Delegates

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        //public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            // System.Action<>
+            // System.Func<>
+
+            var photo = Photo.Load(path);
+
+            //var filters = new PhotoFilters();
+            //filters.ApplyBrightness(photo);
+            //filters.ApplyContrast(photo);
+            //filters.Resize(photo);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+    }
+}
