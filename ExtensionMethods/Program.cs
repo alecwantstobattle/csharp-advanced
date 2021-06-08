30d64fdc5a6faad4e2110dca6d2d0d5af07e3edd
Alec Tecson <tecsonalr@gmail.com>
2021-06-08 16:47:00 +0800

    Extension Methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "The list is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            var max = numbers.Max();
+
+            Console.WriteLine(shortenedPost);
+            Console.WriteLine(max);
+        }
+    }
+
+    //public class RichString : String // sealed
+    //{
+
+    //}
+}
